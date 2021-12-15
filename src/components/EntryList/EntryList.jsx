2509a4b7dcb9a09da7d82b769d1782975199074e
Alice-Retz <alicegretz@gmail.com>
2021-12-14 18:02:21 -0600

    EntryList written

---
+++ src/components/EntryList/EntryList.jsx
@@ -0,0 +1,19 @@
+import Entry from "../Entry/Entry";
+
+export default function EntryList() {
+    const { entries } = useEntries()
+
+    return (
+        <div>
+            <ul>
+                {entries.map((entry) => {
+                    return (
+                        <li key={`${entry.name}-${entry.message}`}>
+                            <Entry entry={entry} />
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
