6185d8e6a205d9a5595d470251d770002600f39e
Alice-Retz <alicegretz@gmail.com>
2022-01-04 16:40:41 -0600

    PrivateRoute.jsx created. New branch and pr for grading purposes.

---
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,21 @@
+import { useUser } from '../../context/UserContext';
+
+export default function PrivateRoute({ children, ...routeProps }) {
+  const { user } = useUser();
+
+  return (
+    <>
+      <p>On a private route?</p>
+      <Route
+        {...routeProps}
+        render={({ location }) =>
+          user ? (
+            children
+          ) : (
+            <Redirect to={{ pathname: '/login', state: { from: location } }} />
+          )
+        }
+      />
+    </>
+  );
+}
